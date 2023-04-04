37756bfaccd6d770a2fbf7d9a3d4d872ed5e66ab
Pobitrodeb <pobitrodn@gmail.com>
2023-04-04 21:37:40 +0600

    basic json formet conscep clear

---
+++ 01 Syntax/main.js
@@ -0,0 +1,17 @@
+// json object formet 
+var student = {
+    'name': 'Pobitro Debnath', 
+    'roll':  '12',
+    'depatment' : 'CSE'
+}
+
+console.log(student);
+
+
+//js forment 
+
+var student_json = {
+    'name' : 'keyamoni', 
+    'roll' : 301983, 
+    'depatment' : 'CSE'
+}
