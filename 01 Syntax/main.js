b72cc4764d0c4f7b932f88e361fd8fc3e3894768
Pobitrodeb <pobitrodn@gmail.com>
2023-04-04 21:52:40 +0600

    convert json to object and object to json

--- 01 Syntax/main.js
+++ 01 Syntax/main.js
@@ -5,13 +5,21 @@ var student = {
     'depatment' : 'CSE'
 }
 
-console.log(student);
 
-
-//js forment 
+//json forment 
 
 var student_json = {
     'name' : 'keyamoni', 
     'roll' : 301983, 
     'depatment' : 'CSE'
 }
+
+//Convert Object to Json 
+
+var convert_student_json_data = JSON.stringify(student); 
+console.log(convert_student_json_data);
+
+
+// Convert to Json 
+var convert_student_object = JSON.parse(convert_student_json_data); 
+console.log(convert_student_object);
