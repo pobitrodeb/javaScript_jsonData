27503d418ccb6539dc981ac7e0d7573c31f4a0f2
Pobitrodeb <pobitrodn@gmail.com>
2023-04-04 23:32:20 +0600

    clear concepet json

---
+++ 03 Warning/main.js
@@ -0,0 +1,15 @@
+var person1 = {
+    name    : "pobitro", 
+    age     : 23, 
+    town    : 'Thakurgon',  
+    marrid  : false, 
+}
+var person2 = {
+    name    : "Deb", 
+    age     : 23, 
+    town    : 'Thakurgon',  
+    marrid  : false, 
+}
+
+var person1 = JSON.stringify(person1); 
+console.log(person1);
