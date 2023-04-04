66da323e1c9d3ab99f810cf302ce0381030659fd
Pobitrodeb <pobitrodn@gmail.com>
2023-04-05 00:16:25 +0600

    convert obj

--- 04 Object And Array/main.js
+++ 04 Object And Array/main.js
@@ -4,8 +4,16 @@ var xmlhtp = new XMLHttpRequest();
 xmlhtp.onreadystatechange = function(){
     if(this.readyState == 4 && this.status == 200)
     {
-    console.log(this.responseText);
+        var data = (this.responseText);
+      
+        jsonData(data);
     }
 }
 xmlhtp.open("Get", "data.json", true); 
 xmlhtp.send(); 
+
+function jsonData(json_object)
+{
+    var javascript_object = JSON.parse(json_object);
+    console.log(javascript_object);
+}
